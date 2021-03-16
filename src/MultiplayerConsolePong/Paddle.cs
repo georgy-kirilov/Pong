880cfa34b06356fbb0a325e1d5687890f6a1d90b
georgy-kirilov <georgynk17@gmail.com>
2021-03-16 11:37:05 +0200

    Initial console screen setup

---
+++ src/MultiplayerConsolePong/Paddle.cs
@@ -0,0 +1,23 @@
+﻿namespace MultiplayerConsolePong
+{
+    public class Paddle
+    {
+        public Paddle(int x, int y, int height)
+        {
+            this.Height = height;
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int Height { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public void MoveUp()
+        {
+            
+        }
+    }
+}
---
+++ src/MultiplayerConsolePong/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace MultiplayerConsolePong
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class Program
+    {
+        private const int MF_BYCOMMAND = 0x00000000;
+        public const int SC_CLOSE = 0xF060;
+        public const int SC_MINIMIZE = 0xF020;
+        public const int SC_MAXIMIZE = 0xF030;
+        public const int SC_SIZE = 0xF000;
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        public static void Main(string[] args)
+        {
+            IntPtr handle = GetConsoleWindow();
+            IntPtr sysMenu = GetSystemMenu(handle, false);
+
+            if (handle != IntPtr.Zero)
+            {
+                DeleteMenu(sysMenu, SC_MINIMIZE, MF_BYCOMMAND);
+                DeleteMenu(sysMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+                DeleteMenu(sysMenu, SC_SIZE, MF_BYCOMMAND);
+            }
+
+            int screenWidth = 100, screenHeight = 28;
+            Console.SetWindowSize(screenWidth, screenHeight);
+            Console.SetBufferSize(screenWidth, screenHeight);
+
+            int ballX = screenWidth / 2, ballY = screenHeight / 2;
+            Console.SetCursorPosition(ballX, ballY);
+            Console.Write("@");
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+            }
+        }
+    }
+}
